981d2edc19192442770c49c6a911280bf7983951
yuelei <devil5050@126.com>
2016-09-02 10:02:50 +0800

    内容同步

--- src/main/java/com/tingo/service/impl/NrSyncService.java
+++ src/main/java/com/tingo/service/impl/NrSyncService.java
@@ -1,10 +1,18 @@
 package com.tingo.service.impl;
 
+import com.tingo.dao.origin.FwOaNrDao;
+import com.tingo.dao.target.DocDetailContentDao;
+import com.tingo.dao.target.DocDetailDao;
+import com.tingo.dto.origin.FwOaNr;
+import com.tingo.dto.target.DocDetail;
 import com.tingo.dto.target.SyncLink;
 import com.tingo.enums.SyncType;
 import com.tingo.service.AbstractSyncService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -13,15 +21,36 @@ import java.util.Map;
  */
 @Service
 public class NrSyncService extends AbstractSyncService {
+    @Autowired
+    private FwOaNrDao originNrDao;
+
+    @Autowired
+    private DocDetailDao docDetailDao;
+
+    @Autowired
+    private DocDetailContentDao docDetailContentDao;
 
     @Override
     public SyncType getSyncType() {
-        return null;
+        return SyncType.nr;
     }
 
     @Override
     public void save(List<String> ids) {
+        if(CollectionUtils.isEmpty(ids)){
+            return;
+        }
 
+        List<FwOaNr> originNrs = originNrDao.selectByIds(ids);
+
+        for(FwOaNr fwOaNr:originNrs){
+            DocDetail detail = bulidDocDetail(fwOaNr);
+        }
+    }
+
+    private DocDetail bulidDocDetail(FwOaNr fwOaNr){
+        DocDetail doc = new DocDetail();
+        doc.
     }
 
     @Override
