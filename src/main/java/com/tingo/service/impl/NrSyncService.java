293b87aa3d1b166cce9b7ad6d1d98d6c538af528
yuelei <devil5050@126.com>
2016-09-02 16:35:38 +0800

    NrSyncService.java add set

--- src/main/java/com/tingo/service/impl/NrSyncService.java
+++ src/main/java/com/tingo/service/impl/NrSyncService.java
@@ -11,6 +11,7 @@ import com.tingo.dto.target.SyncLink;
 import com.tingo.enums.SyncType;
 import com.tingo.service.AbstractSyncService;
 import com.tingo.utils.LocalCache;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -76,7 +77,47 @@ public class NrSyncService extends AbstractSyncService {
             doc.setSeccategory(new BigDecimal(secCategory));
         }
         //todo 添加set代码
-
+        doc.setDocsubject(fwOaNr.getNrbt());
+        doc.setDoctype(new BigDecimal(1));
+        doc.setDoclangurage(new BigDecimal(7));
+        doc.setDocreplyable("0");   //是否允许回复
+        doc.setReplydocid(new BigDecimal(0)); //回复文件id
+        doc.setItemmaincategoryid(new BigDecimal(0));
+        doc.setItemmaincategoryid(new BigDecimal(0));
+        doc.setHrmresid(new BigDecimal(0));
+        doc.setCrmid(new BigDecimal(0));
+        doc.setProjectid(new BigDecimal(0));
+        doc.setFinanceid(new BigDecimal(0));
+        String userId = LocalCache.getInstance().getTargetByOrigin(SyncType.hrm,fwOaNr.getSrry());
+        if(!StringUtils.isEmpty(userId)) {
+            doc.setDoccreaterid(new BigDecimal(userId)); //创建者id
+            doc.setDoclastmoduserid(new BigDecimal(userId)); //最后修改者id
+            doc.setOwnerid(new BigDecimal(userId));
+        }
+        doc.setDocdepartmentid(new BigDecimal(1)); //创建者所在部门
+        doc.setDoccreatedate(DateFormatUtils.format(fwOaNr.getSrsj(),DateFormatUtils.ISO_DATE_FORMAT.getPattern()));
+        doc.setDoccreatetime(DateFormatUtils.format(fwOaNr.getSrsj(),DateFormatUtils.ISO_TIME_FORMAT.getPattern()));
+        doc.setDoclastmoddate(DateFormatUtils.format(fwOaNr.getSrsj(),DateFormatUtils.ISO_DATE_FORMAT.getPattern()));
+        doc.setDoclastmodtime(DateFormatUtils.format(fwOaNr.getSrsj(),DateFormatUtils.ISO_TIME_FORMAT.getPattern()));
+        String shId = LocalCache.getInstance().getTargetByOrigin(SyncType.hrm,fwOaNr.getShry());
+        if(!StringUtils.isEmpty(shId)){
+            doc.setDocapproveuserid(new BigDecimal(shId));
+        }else{
+            doc.setDocapproveuserid(new BigDecimal(0));
+        }
+        doc.setDocarchiveuserid(new BigDecimal(0));
+        //doc.setParentids("0"); //文档父节点
+        doc.setAssetid(new BigDecimal(0));
+        doc.setAccessorycount(new BigDecimal(0)); //附件数量
+        doc.setReplaydoccount(new BigDecimal(0)); //回复文档数量
+        doc.setUsertype("1");
+        doc.setCancopy("1");
+        doc.setCanremind("0"); //回复是否提醒
+        doc.setCountmark(new BigDecimal(0));
+        doc.setSummark(new BigDecimal(0));
+        doc.setDocextendname("html");
+        doc.setInvalidationdate(fwOaNr.getGqrq().toString());
+        doc.setIstop(new BigDecimal(0)); //置顶
         return doc;
     }
 
